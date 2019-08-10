060cca268cd83ed16e4a1314caa3abf6ab55d722
katecc <cat.katecc@gmail.com>
2019-08-10 15:34:13 -0400

    app footer template

---
+++ src/components/Footer/template.vue
@@ -0,0 +1,21 @@
+<style lang="scss" src="./style.scss"></style>
+
+<template>
+  <footer class="app-footer text-center fixed-bottom" :class="{'is-home': isIndexPage}">
+    <div class="container">
+      <div class="content row">
+        <p class="col-md-6">
+          Build by
+          <a href="https://github.com/bertux" target="_blank">Bert0x</a> and
+          <a href="https://github.com/katesblog" target="_blank">KateCC</a>
+        </p>
+        <p class="col-md-6">
+          Project folder:
+          <a href="https://github.com/katesblog/PchainDashboard">
+            <i class="fab fa-github"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
