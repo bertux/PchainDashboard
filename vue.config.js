af37d05bf4ddd00a8ef00b77351efa47309490c1
katecc <cat.katecc@gmail.com>
2019-08-10 15:25:28 -0400

    config style-resources-loader for using scss variable globally

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/imports.scss'),
+      ],
+    });
+}
+
+module.exports = {
+  lintOnSave: false,
+
+  chainWebpack: (config) => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)));
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [],
+    },
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true,
+    },
+  },
+};
